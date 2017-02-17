4b57be60ce08d402cbd25bf0615a37721ad88b91
Zam <zamir.narvaez@correounivalle.edu.co>
2017-02-17 18:49:25 -0500

    prod 0.0.4 = Modificada pagina de error de db , agregado archivo estilo estiloError.css , agregada imagen univalllePalmira.png , traducido mensaje de error de db no pudo conectarse con la confiuracion dada

--- application/errors/error_db.php
+++ application/errors/error_db.php
@@ -1,64 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Database Error</title>
-<style type="text/css">
-
-::selection{ background-color: #E13300; color: white; }
-::moz-selection{ background-color: #E13300; color: white; }
-::webkit-selection{ background-color: #E13300; color: white; }
-
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
-
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	-webkit-box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
-     <?php $this->load->view('plantillas/head'); ?> 
+<title>Error de Base de Datos</title>
+    <link rel="stylesheet" href="../includes/css/semantic.min.css">
+    <link rel="stylesheet" href="../includes/css/estiloError.css">
 </head>
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+
+	<div id="cabecera">
+           	<div id="menu" class="ui borderless menu" >
+     <div class="item">
+        <img class="ui image" src="../includes/images/univallepalmira.png"/>
+    </div>
+    <div class="right menu">
+        <div class="item">
+            <img class="ui medium right floated image" src="../includes/images/manuelita2.png"/>
+        </div>
+    </div>
+		</div> 
+    </div>
+
+
+	 <div class="ui three column centered grid">
+
+	
+
+	 <div class="column">
+		<div id="container" class="ui segment">
+			<h1 class="ui centered header"><?php echo $heading; ?></h1>
+			<div class="ui divider"></div>
+			<?php echo $message; ?>
+		</div>
+	</div>
 	</div>
 </body>
-     <?php $this->load->view('plantillas/footer'); ?> 
+    
 </html>
--- system/language/english/db_lang.php
+++ system/language/english/db_lang.php
@@ -1,7 +1,7 @@
 <?php
 
 $lang['db_invalid_connection_str'] = 'Unable to determine the database settings based on the connection string you submitted.';
-$lang['db_unable_to_connect'] = 'Unable to connect to your database server using the provided settings.';
+$lang['db_unable_to_connect'] = 'No pudo conectarse a su servidor de Base de Datos usando la configuracion dada.';
 $lang['db_unable_to_select'] = 'Unable to select the specified database: %s';
 $lang['db_unable_to_create'] = 'Unable to create the specified database: %s';
 $lang['db_invalid_query'] = 'The query you submitted is not valid.';
@@ -23,7 +23,7 @@ $lang['db_table_name_required'] = 'A table name is required for that operation.'
 $lang['db_column_name_required'] = 'A column name is required for that operation.';
 $lang['db_column_definition_required'] = 'A column definition is required for that operation.';
 $lang['db_unable_to_set_charset'] = 'Unable to set client connection character set: %s';
-$lang['db_error_heading'] = 'A Database Error Occurred';
+$lang['db_error_heading'] = 'Error de Base de Datos';
 
 /* End of file db_lang.php */
 /* Location: ./system/language/english/db_lang.php */
